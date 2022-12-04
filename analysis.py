2d87f5d6990f922047f18582ca3478d9779d240f
DUOLabs333 <dvdugo333@gmail.com>
2022-12-03 22:12:38 -0500

    Rewrite files to answer new question

--- analysis.py
+++ analysis.py
@@ -16,16 +16,25 @@ with shapefile.Reader("DecLands.shp") as reader:
         parks.append(shapely.ops.transform(transformer.transform,shapely.geometry.Polygon(shape.points).convex_hull)) #Use convex hull to avoid self-intersections
     
 
-birds_locations=collections.defaultdict(lambda : [])
 locations=[]
-location_num=0
-
 #Parse each kml file to extract the coordinate boxes for the squares, and what birds were in each box
+
+import shapely.strtree
+
+parks_tree=shapely.strtree.STRtree(parks)
+
 import xml.etree.ElementTree as ET
+import csv
+csvfile=open("analysis_results.csv","w+")
+writer=csv.writer(csvfile)
+writer.writerow(["Location","Birds","Prefer Forests?"])
+location_num=0
 for file in ["north","southeast","west"]:
     tree = ET.parse(f'bba{file}.kml')
     namespaces = {'kml': 'http://www.opengis.net/kml/2.2'}
     for location in tree.iterfind(".//kml:Placemark",namespaces):
+        row=[location_num,[],0]
+        
         coordinates=location.find(".//kml:LinearRing/kml:coordinates",namespaces).text.strip()
         coordinates=coordinates.split(" ")
         coordinates=[_.split(",")[:-1] for _ in coordinates]
@@ -33,56 +42,12 @@ for file in ["north","southeast","west"]:
         fromproj=pyproj.crs.CRS(4326)
         toproj=pyproj.crs.CRS(32632)
         transformer=pyproj.Transformer.from_crs(fromproj,toproj,always_xy=True)
-        locations.append(shapely.ops.transform(transformer.transform,shapely.geometry.Polygon(coordinates)))
+        polygon=shapely.ops.transform(transformer.transform,shapely.geometry.Polygon(coordinates))
         for bird in location.iterfind(".//kml:SimpleData",namespaces):
             if not bird.text.isspace() and bird.text!='NONE': #Only add a park as a location where the bird is found if the bird has been found at that location
-                birds_locations[bird.text].append(location_num)
-        location_num+=1
-
-
-parks_total_area=sum(park.area for park in shapely.ops.unary_union(parks).geoms)
-with shapefile.Reader("State.shp") as reader:
-    fromproj=pyproj.crs.CRS(open("State.prj").read())
-    toproj=pyproj.crs.CRS(32632)
-    transformer=pyproj.Transformer.from_crs(fromproj,toproj,always_xy=True)
-    
-    state_area=shapely.ops.transform(transformer.transform,shapely.geometry.Polygon(reader.shape(0).points)).area-parks_total_area
-
-
-birds_ratios={}
-
-
-import shapely.strtree
-
-parks_tree=shapely.strtree.STRtree(parks)
-
-import pdb
-for bird in birds_locations:
-    inside=0
-    outside=0
-    for location in birds_locations[bird]:
-        location_area=locations[location].area
-        park=shapely.ops.unary_union(parks_tree.query(locations[location]))
-        intersection=park.intersection(locations[location]).area
-        if abs(intersection)<10**(-7):
-            intersection=0
-            
-        if (intersection>location_area-intersection):
-            inside+=location_area
-        else:
-            outside+=location_area
-
-    if outside==0:
-        birds_ratios[bird]="Infinity"
-    else:
-        birds_ratios[bird]=(inside/parks_total_area)/(outside/state_area)
-        if birds_ratios[bird]<10**(-4):
-            birds_ratios[bird]=0
-
-import csv
-
-with open("analysis_results.csv","w+") as csvfile:
-    writer=csv.writer(csvfile)
-    writer.writerow(["Bird","Ratio"])
-    for bird in birds_ratios:
-        writer.writerow([bird,birds_ratios[bird]])
+                row[1].append(bird.text)
+                
+        park=shapely.ops.unary_union(parks_tree.query(polygon))
+        intersection=park.intersection(polygon).area
+        row[2]=int(intersection>polygon.area-intersection) #If more of the location intersected with parks, then it can be considered a forested area
+        writer.writerow(row)
--- compare.py
+++ compare.py
@@ -1,6 +1,6 @@
 import csv
 
-birds_ratios={}
+locations=[]
 
 birds_habitats={}
 
@@ -11,14 +11,13 @@ with open("analysis_results.csv") as f:
     reader=csv.reader(f)
     next(reader)
     for row in reader:
-        birds_ratios[row[0]]=float(row[1])
+        locations.append(row)
 
 with open("preferred_habitats.csv") as f:
     reader=csv.reader(f)
     next(reader)
     for row in reader:
         birds_habitats[row[0]]=row[1]
-
 habitats={
     "Scrub":0,
     "Towns":0,
@@ -31,16 +30,33 @@ habitats={
     "Shorelines":0,
     "Oceans":0
 }
-
-def normalize_score(a): #Function to return score to compare against habitats
-    if a>=1:
-        return 1
-    if a<1 and a>=1:
-        return 2
-    return 0
     
-total=0
-for bird in birds_habitats:
-    total+=int(habitats[birds_habitats[bird]]==normalize_score(birds_ratios[bird]))
+import itertools
+maximum=[[],0]
+for k in itertools.product([0,1,2],repeat=len(habitats)):
+    correct=0
+    total=0
+    habitats=dict(zip(habitats.keys(),k))
+    for location in locations:
+        forest_birds=0
+        open_birds=0
+        for bird in eval(location[1]):
+            if bird not in birds_habitats:
+                continue
+            total+=1
+            habitat=habitats[birds_habitats[bird]]
+            if habitat==0:
+                open_birds+=1
+            elif habitat==1:
+                forest_birds+=1
+            else:
+                open_birds+=1
+                forest_birds+=1
+                
+            if int(location[2])==int(forest_birds>open_birds):
+                correct+=1
+    if (correct/total>maximum[1]):
+        maximum=[k,correct/total]
+        print(maximum)
 
-print(total/len(birds_habitats)*100)
+print((correct/total)*100)
